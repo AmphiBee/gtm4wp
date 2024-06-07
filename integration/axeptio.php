37eb3347f4eaf70b7c900fef8f29707d85650085
Olivier Gorzalka <olivier@amphibee.fr>
2024-06-07 10:58:35 +0200

    Remove google consent mode

--- integration/axeptio.php
+++ integration/axeptio.php
@@ -14,21 +14,6 @@ add_action('wp_head', function () {
 		'clientId' => $gtm4wp_options[GTM4WP_OPTION_INTEGRATE_AXEPTIO . '-projectID'],
 		'cookiesVersion' => $gtm4wp_options[GTM4WP_OPTION_INTEGRATE_AXEPTIO . '-version']
 	);
-
-	if (isset($GLOBALS['gtm4wp_options'][GTM4WP_OPTION_INTEGRATE_CONSENTMODE]) && $GLOBALS['gtm4wp_options'][GTM4WP_OPTION_INTEGRATE_CONSENTMODE]) {
-		$axeptioSettings['googleConsentMode'] = array(
-			'default' => array(
-				'ad_storage' => $gtm4wp_options[ GTM4WP_OPTION_INTEGRATE_CONSENTMODE_ANALYTICS ] ? 'granted' : 'denied',
-				'ad_user_data' => $gtm4wp_options[ GTM4WP_OPTION_INTEGRATE_CONSENTMODE_AD_USER_DATA ] ? 'granted' : 'denied',
-				'ad_personalization' => $gtm4wp_options[ GTM4WP_OPTION_INTEGRATE_CONSENTMODE_AD_PERSO ] ? 'granted' : 'denied',
-				'functionality_storage' => $gtm4wp_options[ GTM4WP_OPTION_INTEGRATE_CONSENTMODE_FUNC ] ? 'granted' : 'denied',
-				'security_storage' => $gtm4wp_options[ GTM4WP_OPTION_INTEGRATE_CONSENTMODE_SECURUTY ] ? 'granted' : 'denied',
-				'personalization_storage' => $gtm4wp_options[ GTM4WP_OPTION_INTEGRATE_CONSENTMODE_PERSO ] ? 'granted' : 'denied',
-				'wait_for_update' => 500
-			)
-		);
-	}
-
 	?>
 	<script>
 		window.axeptioSettings = <?php echo wp_json_encode($axeptioSettings); ?>;
